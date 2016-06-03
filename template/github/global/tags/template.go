8dffc36a7760665f61c09c510506a3062ff0111e
Ciel <imwithye@gmail.com>
2016-06-03 13:46:08 +0800

    add tags to gitignore, fix global ignore

---
+++ template/github/global/tags/template.go
@@ -0,0 +1,29 @@
+package tags
+
+import (
+	"github.com/imwithye/gitignore/template"
+	"strings"
+)
+
+func init() {
+	ignore := []string{
+		"# Ignore tags created by etags, ctags, gtags (GNU global) and cscope",
+		"TAGS",
+		".TAGS",
+		"!TAGS/",
+		"tags",
+		".tags",
+		"!tags/",
+		"gtags.files",
+		"GTAGS",
+		"GRTAGS",
+		"GPATH",
+		"cscope.files",
+		"cscope.out",
+		"cscope.in.out",
+		"cscope.po.out",
+		"",
+		"",
+	}
+	template.Add("github/global/tags", strings.Join(ignore, "\n"))
+}
