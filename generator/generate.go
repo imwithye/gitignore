dc2f0105ffd13d6c5b3aae68e7d7ca40ea2853e4
Ciel <imwithye@gmail.com>
2016-06-03 11:50:02 +0800

    generate gitignore and readme

--- generator/generate.go
+++ generator/generate.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/imwithye/logor"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -13,6 +14,23 @@ import (
 
 const PackageFolder = "template"
 const PackageName = "template"
+
+const GoTemplate = `package %s
+
+import (
+	"strings"
+	"github.com/imwithye/gitignore/template"
+)
+
+func init() {
+	ignore := []string{
+		%s
+	}
+	template.Add("%s", strings.Join(ignore, "\n"))
+}
+`
+
+const ReadmeTemplate = "Git Template %s\n===\n\nUse `git ignore add %s` to add this ignore template.\n\n```\n%s```\n"
 
 func downloadFile(filepath string, url string) (err error) {
 	out, err := os.Create(filepath)
@@ -72,16 +90,43 @@ func visitFile(path string, f os.FileInfo, err error) error {
 
 func generateCode(path string) {
 	log := logor.GetLogor()
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Error("   ", err)
+		return
+	}
+	ignores := []string{}
+	for _, line := range strings.Split(string(content), "\n") {
+		line = strings.Replace(line, "\\", "\\\\", -1)
+		line = strings.Replace(line, "\"", "\\\"", -1)
+		ignores = append(ignores, fmt.Sprintf("\"%s\",", line))
+	}
 	relPath, err := filepath.Rel("gitignore-master", path)
 	if err != nil {
-		log.Fatal("   ", err)
+		log.Error("   ", err)
 		return
 	}
 	extension := filepath.Ext(relPath)
-	packagePath := filepath.Join(PackageFolder, relPath[0:len(relPath)-len(extension)])
+	packagePath := filepath.Join(PackageFolder, "github", relPath[0:len(relPath)-len(extension)])
 	packagePath = strings.ToLower(packagePath)
 	packagePath = strings.Replace(packagePath, "+", "p", -1)
 	packagePath = strings.Replace(packagePath, "-", "_", -1)
+	packageName := filepath.Base(packagePath)
+	err = os.MkdirAll(packagePath, 0755)
+	if err != nil {
+		log.Error("   ", err)
+		return
+	}
+	goFilePath := filepath.Join(packagePath, "template.go")
+	gocode := fmt.Sprintf(
+		GoTemplate, packageName,
+		strings.Join(ignores, "\n"),
+		strings.ToLower(filepath.Join("github", relPath[0:len(relPath)-len(extension)])))
+	ioutil.WriteFile(goFilePath, []byte(gocode), 0644)
+	readmeFilePath := filepath.Join(packagePath, "README.md")
+	readme := fmt.Sprintf(ReadmeTemplate, relPath[0:len(relPath)-len(extension)], packageName, string(content))
+	ioutil.WriteFile(readmeFilePath, []byte(readme), 0644)
+	// importPath := filepath.Join("github.com/imwithye/gitignore", packagePath)
 	fmt.Println(packagePath)
 }
 
@@ -89,16 +134,32 @@ func main() {
 	log := logor.GetLogor()
 	var err error
 	log.Info("-->", "Downloading Github Gitignore Template")
+	err = os.RemoveAll("gitignore-master.zip")
+	if err != nil {
+		log.Fatal("-->", err)
+	}
 	err = downloadFile("gitignore-master.zip", "https://codeload.github.com/github/gitignore/zip/master")
 	if err != nil {
 		log.Fatal("-->", err)
 	}
 	log.Info("-->", "Extracting Github Gitignore Template")
+	err = os.RemoveAll("gitignore-master")
+	if err != nil {
+		log.Fatal("-->", err)
+	}
 	err = unzipFile("gitignore-master.zip", ".")
 	if err != nil {
 		log.Fatal("-->", err)
 	}
 	log.Info("-->", "Generating Go Codes & Packages")
+	err = os.RemoveAll("template/github")
+	if err != nil {
+		log.Fatal("-->", err)
+	}
+	err = os.RemoveAll("template/all")
+	if err != nil {
+		log.Fatal("-->", err)
+	}
 	err = filepath.Walk("gitignore-master", visitFile)
 	if err != nil {
 		log.Fatal("-->", err)
