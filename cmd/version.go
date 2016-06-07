6992d7565f8fb2ddff69eb916d89ebcd4ae22e92
Ciel <imwithye@gmail.com>
2016-06-08 00:07:22 +0800

    set version to 1.0.1

--- cmd/version.go
+++ cmd/version.go
@@ -6,11 +6,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const Version = "v1.0.1"
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s, %s, %s\n", "v1.0.0", platform.Platform, "Ciel <me@ciel.im>")
+		fmt.Printf("%s, %s, %s\n", Version, platform.Platform, "Ciel <me@ciel.im>")
 	},
 }
 
