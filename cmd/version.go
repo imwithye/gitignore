96c07c3fa4f6f8b1c034d9896b61032d85c26403
Ciel <imwithye@gmail.com>
2016-06-08 23:33:07 +0800

    add no check flag to version cmd

--- cmd/version.go
+++ cmd/version.go
@@ -7,19 +7,25 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var versionCmdNoCheck bool
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("%s, %s, %s\n", version.CurrentVersion, platform.Platform, "Ciel <me@ciel.im>")
-		latestVersion, url, err := version.Latest()
-		if err != nil || version.CurrentVersion.Cmp(latestVersion) > 0 {
+		fmt.Printf("%s, %s, %s.\n", version.CurrentVersion, platform.Platform, "Ciel <me@ciel.im>")
+		if versionCmdNoCheck {
 			return
 		}
-		fmt.Printf("latest version %s, try download from %s\n", latestVersion, url)
+		latestVersion, url, err := version.Latest()
+		if err != nil || version.CurrentVersion.Cmp(latestVersion) > 0 {
+			fmt.Println("Already up-to-date.")
+			return
+		}
+		fmt.Printf("Latest version %s, %s\n", latestVersion, url)
 	},
 }
 
 func init() {
+	versionCmd.Flags().BoolVarP(&versionCmdNoCheck, "no-check", "", false, "don't check latest version")
 	RootCmd.AddCommand(versionCmd)
 }
