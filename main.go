a95e6af6b4ea610f54d24d9b0fd85816303dc139
Yiwei Gong <imwithye@gmail.com>
2021-11-09 22:43:20 +0800

    fmt gitignore file

--- main.go
+++ main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"embed"
 	"fmt"
-	"io/fs"
-	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 )
 
 //go:embed templates
 var templates embed.FS
 
-func main() {
-	args := os.Args[1:]
+func gitignore(args []string) string {
 	var ret []string
 	for _, arg := range args {
+		ret = append(ret, fmt.Sprintf("# Gitignore %s", arg))
 		data, err := templates.ReadFile(fmt.Sprintf("templates/%v.gitignore", arg))
 		if err != nil {
 			continue
 		}
 		ret = append(ret, string(data))
+		ret = append(ret, "\n")
 	}
-	ioutil.WriteFile(".gitignore", []byte(strings.Join(ret, "\n")), fs.ModeAppend)
+	str := strings.Join(ret, "\n")
+	str = strings.ReplaceAll(str, "\r", "\n")
+	reg := regexp.MustCompile(`([\n]{3})+`)
+	str = reg.ReplaceAllString(str, "\n\n")
+	str = strings.ReplaceAll(str, "\n\n", "\n")
+	reg = regexp.MustCompile(`\n*$`)
+	str = reg.ReplaceAllString(str, "\n")
+	return str
+}
+
+func main() {
+	args := os.Args[1:]
+	fmt.Println(gitignore(args))
 }
